230280eb2246c998e48df1d8f234e423a114c106
DESKTOP-3GBEFFK\DREAMS03 <03dreams@naver.com>
2024-01-15 20:57:47 +0900

    240115-입출금 계산기 만들었는데 잘 모르겠다,,

--- js/06_입출금하기.js
+++ js/06_입출금하기.js
@@ -4,20 +4,38 @@ const amount = [""];                  // 사용자로부터 금액 입력 받음
 let balance = 10000;                         // 잔액 기록 변수 (초기값 10000)
 const password = '1234';                      // 비밀번호 저장 변수 (초기 비밀번호 1234)
 
+const resultIn = document.getElementById("resultIn");
+const resultOut = document.getElementById("resultOut");
+
+// function deposit() {
+//     let amount = document.getElementById("amount");
+//     if ( amount >=0 ) {
+//         const amount = prompt("입금되었습니다");
+//     } else {
+//         alert("금액을 다시 확인해주세요");
+//     }
+// }
+
 function deposit() {
-    
+    const depositAmount = Number(document.getElementById('amount').value);
+
+    if (!(depositAmount) || depositAmount <= 0) {
+        alert("올바른 금액을 입력하세요.");
+    }
+
+    balance += depositAmount;
+    alert(`${depositAmount}원이 입금 되었습니다. 현재 잔액: ${balance}원`);
+
+
 }
 
-
-
-
-
-
 function withdrawal() {
+
+    const withdrawalAmount = Number(document.getElementById('amount').value);
     const password = prompt("비밀번호를 입력하세요");
 
-    const amount = document.getElementById("amount")
-    let balance = document.getElementById("balance")
+    const amount = document.getElementById("amount");
+    let balance = document.getElementById("balance");
 
     // 확인 -> 입력한 문자열
     // 취소 -> null
@@ -30,17 +48,18 @@ function withdrawal() {
         if (password == '1234') {
              alert("비밀번호가 일치");
          
-        if (balance > resultIn ){
-            alert("출금 금액이 잔액보다 클 수 없습니다");
-
-        } else {
-            alert(" _원이 출금 되었습니다.  남은 잔액 _원");
-        }
+             if (withdrawalAmount > balance) {
+                alert("출금 금액이 잔액보다 클 수 없습니다.");
+            } else {
+                balance -= withdrawalAmount;
+                alert(`${withdrawalAmount}원이 출금 되었습니다. 남은 잔액: ${balance}원`);
+            }
         }else {
             alert("비밀번호가 일치하지 않습니다.");
         }
     }
     
 }
+
 
 
