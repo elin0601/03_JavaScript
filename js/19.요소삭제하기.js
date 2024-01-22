9b7da244a234596dccf7a5d4340168130051bd4f
dlwltn <03dreams@naver.com>
2024-01-22 17:22:54 +0900

    240122 공부한 것들

---
+++ js/19.요소삭제하기.js
@@ -0,0 +1,60 @@
+
+const addBtn = document.querySelector("#addBtn");
+const calBtn = document.querySelector("#calBtn");
+const area = document.querySelector(".area");
+
+addBtn.addEventListener("click", ()=>{
+    const container = document.createElement("div");
+
+    container.classList.add("container");
+    area.append(container);
+
+    const input = document.createElement("input");
+    input.type = "number";
+
+    container.append(input);   
+
+
+
+
+    /* 1.로딩, 이벤트 발생 등 시점에 따라서 선택되는 요소가 달라진다
+       2. x버튼 처럼 새롭게 만들어지는 요소(동적 추가 요소)한테 클릭 이벤트를
+       추가해줘야 한다. */
+    const span = document.createElement("span");
+  
+  
+    span.classList.add("remove-row");
+  
+  
+    span.innerHTML = "&times;";
+  
+  
+    span.addEventListener("click", e => {
+  
+  
+    const parent = e.target.parentElement;
+  
+  
+    parent.remove();
+});
+
+container.append(input, span);
+
+
+area.append(container);
+});
+
+
+
+calBtn.addEventListener("click", ()=>{
+
+  const numbers = document.querySelectorAll(".input");
+
+  let sum = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    sum += Number(numbers[i].value);
+  }
+
+  alert("결과 : " + sum);
+});
