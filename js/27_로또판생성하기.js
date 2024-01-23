9c014fe9ba31100ce9b168ee4e149a07cca1a3a2
DESKTOP-3GBEFFK\DREAMS03 <03dreams@naver.com>
2024-01-23 20:45:30 +0900

    240123

--- js/27_로또판생성하기.js
+++ js/27_로또판생성하기.js
@@ -7,7 +7,20 @@ const numContainer = document.querySelector(".numContainer");
     - 최대 6개 선택
     - 선택 취소 가능
 */
+function RandomNumber() {
+    return Math.floor(Math.random() * 45) + 1;
+}
 
 lottoBtn.addEventListener("click", e=>{
     numContainer.innerHTML = "";
-}); 
+});
+
+for (let i = 0; i < 6; i++) {
+    const randomNumber = RandomNumber();
+
+    // Create a new div element for each number
+    const numberDiv = document.createElement('div');
+    numberDiv.classList.add('number');
+    numberDiv.textContent = randomNumber;
+}
+
