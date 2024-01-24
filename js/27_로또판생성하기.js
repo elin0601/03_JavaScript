3ecdb0a62fbffa4b9f599c5bbb5b97bb7e390127
dlwltn <03dreams@naver.com>
2024-01-24 11:31:42 +0900

    240124_JS 총정리

--- js/27_로또판생성하기.js
+++ js/27_로또판생성하기.js
@@ -1,26 +1,58 @@
-const lottoBtn = document.querySelector("#lottoBtn");
-const container = document.querySelector("#container");
-const numContainer = document.querySelector(".numContainer");
+const createBtn = document.querySelector("#createBtn");
+const lottoBoard = document.querySelector("#lottoBoard");
 
-/*  
-    - 생성 버튼 클릭 시 이전 로또판 삭제 -> 새 로또판 생성
-    - 최대 6개 선택
-    - 선택 취소 가능
-*/
-function RandomNumber() {
-    return Math.floor(Math.random() * 45) + 1;
-}
+// #createBtn 버튼 클릭 시
+// #lottoBoard에 div.number 요소 45개 생성해서 추가하기
+createBtn.addEventListener("click", () => {
 
-lottoBtn.addEventListener("click", e=>{
-    numContainer.innerHTML = "";
+    // #iottoBoard 이전 내용 삭제
+    lottoBoard.innerHTML="";
+
+    // div.number 만들어서 추가
+  for(let i=0 ; i<45 ; i++){
+    const number = document.createElement("div");
+
+    number.classList.add("number");  // 클래스 추가
+   
+    number.addEventListener("click", e=>{
+        
+        // active 클래스 없으면 -> 추가
+        // active 클래스 있으면 -> 제거
+
+        /* 요소.classList.contains(클래스명) : 요소에(클래스명)이 존재하면 true, 아님 false */
+
+        /* if(e.target.classList.contains("active")){
+            e.target.classList.remove("active");
+        } else {
+            e.target.classList.add("active");
+        } */
+
+        //  클릭 전에 화면에 선택된 요소가 5개 이하일 경우
+        // 또는
+        // 클릭한 요소에 active 클래스가 있을 경우
+        const count = document.querySelectorAll(".active").length;
+
+        if(count <= 5 || e.target.classList.contains("active")) {
+
+        
+        // console.log(count);
+
+        /* 요소.classList.toggle(클래스명) : 요소에 (클래스명)이 있으면 제거, 없으면 추가*/
+        e.target.classList.toggle("active");
+        } 
+        else{
+            alert("6개 까지만 선택할 수 있습니다");
+        }
+    });
+
+
+
+    number.textContent = i +1;
+
+    lottoBoard.append(number);
+  }
+
+
+
+
 });
-
-for (let i = 0; i < 6; i++) {
-    const randomNumber = RandomNumber();
-
-    // Create a new div element for each number
-    const numberDiv = document.createElement('div');
-    numberDiv.classList.add('number');
-    numberDiv.textContent = randomNumber;
-}
-
